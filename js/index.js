07a42a4c56861c51b69aaa8141af9b17cd4bffe9
HanneloreMaes <71761907+HanneloreMaes@users.noreply.github.com>
2021-07-12 11:54:20 +0200

    Start oef !!NIET VOOR PUNTEN ALS OEF!!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const ehb = {
+    init(){
+
+    },
+
+    render() {
+        fetch(`https://thecrew.cc/news/read.php`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("News", data);
+                data.forEach(function (news) {
+                    const containerNews = document.getElementById('container');
+                });
+            });
+    },
+};
+
+ehb.init();
